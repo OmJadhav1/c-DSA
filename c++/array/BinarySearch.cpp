306c978783177dc8376cb1b25d636b4738368c10
omJadhav1 <omjadhav0462@gmail.com>
2022-11-08 11:51:30 +0530

    updated code of binary search

--- c++/array/BinarySearch.cpp
+++ c++/array/BinarySearch.cpp
@@ -1,41 +1,43 @@
 #include <iostream>
 using namespace std;
 
-int BinearySearch(int arr[], int n , int key){
+int BinearySearch(int arr[], int n, int key)
+{
 
-    int s =0;
-    int e =n-1;
-    
+    int s = 0;
+    int e = n - 1;
 
-    while(s<=e){
-        int mid= (s+e)/2;
+    while (s <= e)
+    {
+        int mid = s + (e - s) / 2;
 
-        if(arr[mid]==key){
+        if (arr[mid] == key)
+        {
             return mid;
-            
         }
-        else if(arr[mid]>key){
-            e=mid-1;
+        else if (arr[mid] > key)
+        {
+            e = mid - 1;
         }
-        else{
-            s= mid+1;
+        else
+        {
+            s = mid + 1;
         }
-        
     }
     return -1;
 }
-int main() {
+int main()
+{
 
-    int arr[]={10,20,30,40,50,60};
-    int n= sizeof(arr)/sizeof(int);
+    int arr[] = {10, 20, 30, 40, 50, 60};
+    int n = sizeof(arr) / sizeof(int);
 
     int key;
-    cout<<"enter element to find"<<endl;
-    cin>>key;
+    cout << "enter element to find" << endl;
+    cin >> key;
 
-    int index =BinearySearch(arr , n, key);
-    cout<<index;
+    int index = BinearySearch(arr, n, key);
+    cout << index;
 
-
-     return 0;
+    return 0;
 }
